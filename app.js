b0aedaf351c0e24ba80d59651c3d4693b5888ee3
Arvind Shelke <ucdonor5@gmail.com>
2021-08-19 15:21:41 +0530

    form post for signup done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request=require("request");
 const parser=require("body-parser");
 const app=express();
 
-// app.use(parser.urlencoded({extended:true}));
+app.use(parser.urlencoded({extended:true}));
 app.use(express.static("public"))
 
 app.get("/", function(req, res){
@@ -12,20 +12,13 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    const city=req.body.cityName
-    let cityname=req.body.cityName
-    const apiKey='543c762d659ecb301bd6305cfae9350f'
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+ cityname +"&APPID="+ apiKey +""
-
-    https.get(url, function(resp){
-        resp.on("data", (data)=>{
-        const weatherData=JSON.parse(data)
-        const iconUrl="http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png"
-        res.write("<p>"+weatherData.weather[0].description+"</p>")
-        res.write("<img src="+iconUrl+">")
-        res.send()
-        });        
-    });
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+    const emailInput=req.body.emailInput;
+    res.write(firstName);
+    res.write(lastName);
+    res.write(emailInput);
+    res.send();
 });
 app.listen(8000, ()=>{
     console.log("Running on port 8000.")
