096e42a039f6182993175c85f5df4de177210106
Arvind Shelke <ucdonor5@gmail.com>
2021-08-19 16:11:46 +0530

    keys config done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const { json } = require("express");
 const express=require("express");
 const request=require("request");
+const https=require("https");
 const parser=require("body-parser");
 const app=express();
 
@@ -15,11 +16,37 @@ app.post("/", function(req, res){
     const firstName=req.body.firstName;
     const lastName=req.body.lastName;
     const emailInput=req.body.emailInput;
-    res.write(firstName);
-    res.write(lastName);
-    res.write(emailInput);
-    res.send();
+    const data={
+        members:[
+            {
+                email_address:emailInput,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+    const jsonData=JSON.stringify(data);
+    const url="https://us5.api.mailchimp.com/3.0/lists/a4e55816bb"
+    const options={
+        method:"POST",
+        auth:"arvi_:a869d02ebe6bfcb4907db74c78f15c63-us5"
+    }
+    const request=https.request(url, options, (r)=>{
+        r.on("data", ()=>{
+            console.log(JSON.parse(data))
+        });
+    });
+    request.write(jsonData);
+    request.end();
 });
 app.listen(8000, ()=>{
     console.log("Running on port 8000.")
 }); 
+
+//a869d02ebe6bfcb4907db74c78f15c63-us5
+
+
+//a4e55816bb
