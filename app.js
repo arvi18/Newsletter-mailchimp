5aba08fc80ab1c1a0814462690aaee2ae91cb0e8
Arvind Shelke <ucdonor5@gmail.com>
2021-08-19 19:22:32 +0530

    succes done

--- app.js
+++ app.js
@@ -36,9 +36,17 @@ app.post("/", function(req, res){
     }
     const request=https.request(url, options, (r)=>{
         r.on("data", ()=>{
-            console.log(JSON.parse(data))
+            console.log(r.statusCode)
+            if (r.statusCode===200){
+                res.sendFile(__dirname+"/success.html")
+
+            }else{
+                res.sendFile(__dirname+"/failure.html")
+            }
         });
     });
+    console.log(jsonData)
+
     request.write(jsonData);
     request.end();
 });
