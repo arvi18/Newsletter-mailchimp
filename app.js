03cc61eff1ee9cef25f5dfa01e66c819ab032a76
Arvind Shelke <ucdonor5@gmail.com>
2021-08-19 19:55:11 +0530

    process.env

--- app.js
+++ app.js
@@ -29,14 +29,16 @@ app.post("/", function(req, res){
         ]
     };
     const jsonData=JSON.stringify(data);
-    const url="https://us5.api.mailchimp.com/3.0/lists/a4e55816bb"
+    const api_id=process.env.api_id;
+    const api_key=process.env.api_key;
+    const url="https://us5.api.mailchimp.com/3.0/lists/"+ api_id;
     const options={
         method:"POST",
-        auth:"arvi_:a869d02ebe6bfcb4907db74c78f15c63-us5"
+        auth:"arvi_:"+api_key
     }
     const request=https.request(url, options, (r)=>{
         r.on("data", ()=>{
-            console.log(r.statusCode)
+            console.log(r)
             if (r.statusCode===200){
                 res.sendFile(__dirname+"/success.html")
 
@@ -45,8 +47,6 @@ app.post("/", function(req, res){
             }
         });
     });
-    console.log(jsonData)
-
     request.write(jsonData);
     request.end();
 });
